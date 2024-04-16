354d7f1fe58f6659a0c3ad6d67b12a4747f6efeb
mirzayev otbek <otabek.@gmail.com>
2024-04-16 19:36:49 +0300

    twit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="align-element">
+      <h1>Home</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus labore eaque delectus est eius, expedita vero ipsum provident amet et qui, perferendis corrupti id dolore illum impedit optio maiores velit!</p>
+    </div>
+  )
+}
+
+export default Home
